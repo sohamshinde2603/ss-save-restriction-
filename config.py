9715c7e8f2c195385a3a54d665815261bd9d17ce
sohamshinde2603 <135973055+sohamshinde2603@users.noreply.github.com>
2025-08-13 23:30:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,9 +30,10 @@ MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for s
 IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "10"))
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "1000"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
+
 
 
